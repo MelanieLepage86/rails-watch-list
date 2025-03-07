9d7e248a73d4be252fca50d2b47fa3d37c9aa729
melanie lepage <melanie.lepage86@hotmail.fr>
2025-03-07 17:51:31 +0100

    watchList => Cloudinary MAJ

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  root "lists#index"
+  root "/lists#index"
   resources :lists, only: %i[index show new create destroy] do
     resources :bookmarks, only: %i[new create destroy]
   end

7e1586c2d80055afffa80ccd6d780c4ebf7cb20e
melanie lepage <melanie.lepage86@hotmail.fr>
2025-03-07 16:03:29 +0100

    WatchList => Maj

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,12 +7,27 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
-5.times do
-  movie = Movie.new(
-    title: Faker::Kpop.boy_bands,
-    overview: Faker::Movies::Hobbit.quote,
-    poster_url: Faker::LoremFlickr.image,
-    rating: Faker::Number.within(range: 0..10)
-  )
-  movie.save
+require "open-uri"
+require "json"
+
+puts "Cleaning up database..."
+Bookmark.destroy_all
+List.destroy_all
+Movie.destroy_all
+puts "Database cleaned"
+
+
+url = "https://tmdb.lewagon.com/movie/top_rated";
+
+10.times do
+  movies = JSON.parse(URI.open(url).read)["results"]
+
+  movies.each do |movie|
+     Movie.create(
+      title: movie["title"],
+      overview: movie["overview"],
+      poster_url: "https://image.tmdb.org/t/p/w500#{movie["poster_path"]}",
+      rating: movie["vote_average"]
+    )
+  end
 end
